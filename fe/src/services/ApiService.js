8453a435e10c6aca2cb683b2fc5a594f18c53241
filip-ljubotina <filip.ljubotina@fer.hr>
2023-11-12 00:26:01 +0100

    deploy update

--- fe/src/services/ApiService.js
+++ fe/src/services/ApiService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const { API_BASE_URL } = process.env;
+
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: API_BASE_URL,
     credentials: 'include',
     headers: {
         'Content-Type': 'application/json',
