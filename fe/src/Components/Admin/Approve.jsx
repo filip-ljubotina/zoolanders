839f74c28f81ad24a3a2b9f51a3b92b0bee0d25a
ana-vuksanovic <148348445+ana-vuksanovic@users.noreply.github.com>
2023-11-17 21:57:04 +0100

    minor fix

--- fe/src/Components/Admin/Approve.jsx
+++ fe/src/Components/Admin/Approve.jsx
@@ -5,7 +5,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import ApiService from '../../Services/ApiService';
+import ApiService from '../../services/ApiService';
 import PropTypes from 'prop-types';
 
 export default function Approve({row, onApprove}) {
