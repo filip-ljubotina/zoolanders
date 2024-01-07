5866d6df9f97173919cbc59de5178fb5c7641d50
filip-ljubotina <148492574+filip-ljubotina@users.noreply.github.com>
2024-01-07 20:42:41 +0100

    Update SearchTypeMap.jsx

--- fe/src/Components/Manager/SearchTypeMap.jsx
+++ fe/src/Components/Manager/SearchTypeMap.jsx
@@ -1,5 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup, Polygon} from 'react-leaflet';
+import L from 'leaflet';
 import PropTypes from 'prop-types';
+import home_icon_png from '../Assets/home.png'
 
 const SearchTypeMap = ({ coordinates, searchType }) => {
   const polygonOpacity = {
@@ -20,7 +22,11 @@ const SearchTypeMap = ({ coordinates, searchType }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={coordinates["center"]}>
+      <Marker position={coordinates["center"]} icon={L.icon({
+            iconUrl: home_icon_png,
+            iconSize: [32, 32],
+            iconAnchor: [16, 32],
+          })}>
         <Popup>Kordinate Postaje: {coordinates["center"][0]}, {coordinates["center"][1]}</Popup>
       </Marker>
       {searchType && <Polygon key={searchType} positions={polygon} fillOpacity={opacity} color="blue" />}
